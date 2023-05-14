16f3d60999c860fa862006a95df810c42faa26cc
anshuman-coder <anshurockingboy23@gmail.com>
2023-05-14 15:27:00 +0530

    this is dependency injection commit, and the code explanation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
-@Module({})
-export class AuthModule {}
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService]
+})
+export class AuthModule {
+  
+}

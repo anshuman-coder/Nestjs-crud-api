030a0bc6ab0394595ccb20dc2b03a1f6e1e6e1e1
anshuman-coder <anshurockingboy23@gmail.com>
2023-05-17 22:18:57 +0530

    added the password field to the using issue rectified

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaSerive } from 'src/prisma/prisma.service';
 import { AuthLoginDto } from './dto';
+import crypto from "crypto"
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaSerive) { 
+  constructor(private prisma: PrismaSerive) { }
 
-  }
-  login(body: AuthLoginDto) { 
+  async login(body: AuthLoginDto) { 
+    //create the password hash
+    const hash = crypto.createHash("md5").update(body.password).digest('hex');
+
+    //create the new user using the prisma
+    
+
+    //return the new user which has been created
     return body
   }
 
